e8e242fb44fe0933d9aa9353b4ac5319c7e5626d
wufei <u9W88z6254>
2018-07-02 09:19:46 +0800

    删除没用的 组件

--- app/Pages/Profile/ShopCenter/SelectDoctorMobxStore.js
+++
@@ -1,9 +0,0 @@
-import {observable, action, computed, autorun} from 'mobx';
-
-export default class SelectDoctorMobxStore {
-    //登录信息
-    @observable
-    DoctorInfo = '';
-}
-
-
--- app/store/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wufei on 2018/5/24.
- */
-import account from './common/Account'
-
-export default {
-    account
-}
